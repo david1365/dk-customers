5281c8bb5c6ffc492e2219e18bae2ab86d284d66
davood <daak1365@yahoo.com>
2018-04-11 12:25:58 +0430

    customers sample

---
+++ customer-list/customer-list.component.js
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+var CustomerListComponent = /** @class */ (function () {
+    function CustomerListComponent() {
+    }
+    CustomerListComponent.prototype.ngOnInit = function () {
+    };
+    CustomerListComponent.decorators = [
+        { type: Component, args: [{
+                    selector: 'app-customer-list',
+                    templateUrl: './customer-list.component.html',
+                    styleUrls: ['./customer-list.component.css']
+                },] },
+    ];
+    /** @nocollapse */
+    CustomerListComponent.ctorParameters = function () { return []; };
+    return CustomerListComponent;
+}());
+export { CustomerListComponent };
+//# sourceMappingURL=customer-list.component.js.map
